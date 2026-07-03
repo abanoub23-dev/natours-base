aa370a52c09b5ea3a1d6d5593710fc2733bf167a
abanoub23-dev <abanoub.mansour.gabra@gmail.com>
2026-07-03 04:17:37 +0300

    improve stirpe implement

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -66,7 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    const event = stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET,
